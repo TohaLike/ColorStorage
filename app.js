8753391df27785c834c8ab28bffbc14890794b4a
Anton Malyshev <90730699+TohaLike@users.noreply.github.com>
2022-07-08 14:59:51 +0300

    i create button remove

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const indexColor = document.querySelector('.index__color');
+const colorBackground = document.querySelector('.colorBackground');
 
 let colorStorage = [];
 
 function change() {
     let changeIndex = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F'];
     let setIndex = '';
-    
-
     for (let i = 0; i <= 5; i++) {
         let colorIndex = Math.floor(Math.random() * changeIndex.length)
         setIndex += changeIndex[colorIndex]; 
     };
-
     let colorSafe = {
         color: setIndex
     };
-    
     indexColor.innerHTML = colorSafe.color;
-    colorStorage.push(colorSafe);
-    document.getElementById('colorBackgroundBlock').style.backgroundColor = '#' + colorSafe.color;
-    console.log(colorStorage);
+    colorStorage.push(colorSafe.color);
+    colorBackground.style.backgroundColor = '#' + colorStorage[colorStorage.length - 1];
 };
 
-
-function remove(index) {
-    colorStorage.forEach(() => {
-        if(colorStorage[colorStorage.length-1]){
-            colorStorage.splice(index, 1)
-            console.log(colorStorage)
-        }
-    })
-        
-}
+const remove = (index) => {
+    if (!colorStorage.length) return;
+    colorStorage.pop(index, 1);
+    indexColor.innerHTML = colorStorage[colorStorage.length - 1];
+    colorBackground.style.backgroundColor = '#' + colorStorage[colorStorage.length - 1];
+    if (colorStorage.length === 0) {
+        indexColor.innerHTML = '00000';
+        colorBackground.style.backgroundColor = 'white';
+    };
+};
 

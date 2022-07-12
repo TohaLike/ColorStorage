09db23728005cb7a7ecdf446e8b36d714d2691f6
Anton Malyshev <90730699+TohaLike@users.noreply.github.com>
2022-07-12 12:55:48 +0300

    i add localStorage for save current color

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const colorBackground = document.querySelector('.colorBackground');
 
 let colorStorage = [];
 
+if (localStorage.getItem('color')) {
+    colorStorage = JSON.parse(localStorage.getItem('color'));
+    indexColor.innerHTML = colorStorage[colorStorage.length - 1];
+    colorBackground.style.backgroundColor = '#' + colorStorage[colorStorage.length - 1];
+    if (colorStorage.length === 0) {
+        indexColor.innerHTML = '00000';
+    };
+};
+
 function change() {
     let changeIndex = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F'];
     let setIndex = '';
     for (let i = 0; i <= 5; i++) {
-        let colorIndex = Math.floor(Math.random() * changeIndex.length)
+        let colorIndex = Math.floor(Math.random() * changeIndex.length);
         setIndex += changeIndex[colorIndex]; 
     };
     let colorSafe = {
@@ -16,6 +25,7 @@ function change() {
     indexColor.innerHTML = colorSafe.color;
     colorStorage.push(colorSafe.color);
     colorBackground.style.backgroundColor = '#' + colorStorage[colorStorage.length - 1];
+    localStorage.setItem('color', JSON.stringify(colorStorage));
 };
 
 const remove = (index) => {
@@ -27,5 +37,6 @@ const remove = (index) => {
         indexColor.innerHTML = '00000';
         colorBackground.style.backgroundColor = 'white';
     };
+    localStorage.setItem('color', JSON.stringify(colorStorage));
 };
 
